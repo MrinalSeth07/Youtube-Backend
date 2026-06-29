2ee87ff5982802466f56f71619fc07db00121d3d
MrinalSeth07 <mrinalseth13331@gmail.com>
2026-06-29 14:25:04 +0530

    Fix formatting in subscriptions model file by ensuring consistent spacing

--- src/models/subscriptions.model.js
+++ src/models/subscriptions.model.js
@@ -13,4 +13,4 @@ const subscriptionsSchema = new Schema({
     timestamps: true,
 }
 )
-export const Subscription = mongoose.model("Subscription", subscriptionsSchema )
+export const Subscription =  mongoose.model("Subscription", subscriptionsSchema )
